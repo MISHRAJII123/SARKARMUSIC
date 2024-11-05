27082018f7989e116ee578172c37c344d7c24013
MISHRAJII123 <ccaddnew@gmail.com>
2024-11-05 19:44:56 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -1,25 +1,10 @@
 from VIPMUSIC import app
-from pyrogram import Client, filters
-from pyrogram.enums import ChatMemberStatus
-from pyrogram.errors import (
-    ChatAdminRequired,
-    InviteRequestSent,
-    UserAlreadyParticipant,
-    UserNotParticipant,
-)
 from pyrogram.errors import RPCError
 from pyrogram.types import ChatMemberUpdated, InlineKeyboardMarkup, InlineKeyboardButton
 from os import environ
 from typing import Union, Optional
 from PIL import Image, ImageDraw, ImageFont
 from os import environ
-import requests
-import random
-from VIPMUSIC import app, userbot
-from VIPMUSIC.misc import SUDOERS
-from pyrogram import * 
-from pyrogram.types import *
-from VIPMUSIC.utils.vip_ban import admin_filter
 import random
 from pyrogram import Client, filters
 from pyrogram.types import ChatJoinRequest, InlineKeyboardButton, InlineKeyboardMarkup
@@ -30,12 +15,8 @@ from PIL import Image, ImageDraw, ImageFont, ImageEnhance
 from asyncio import sleep
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
-from pyrogram import *
-from pyrogram.types import *
 from logging import getLogger
 from VIPMUSIC.utils.vip_ban import admin_filter
-import os
-from VIPMUSIC.misc import SUDOERS
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
@@ -97,15 +78,22 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("VIPMUSIC/assets/wel2.png")
+    background = Image.open("assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
-    pfp = pfp.resize((825, 824))
+    pfp = pfp.resize((575, 575))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('VIPMUSIC/assets/font.ttf', size=110)
-    welcome_font = ImageFont.truetype('VIPMUSIC/assets/font.ttf', size=60)
-    draw.text((2100, 1420), f'ID: {id}', fill=(12000, 12000, 12000), font=font)
-    pfp_position = (1990, 435)
+    font = ImageFont.truetype('assets/font.ttf', size=70)
+    welcome_font = ImageFont.truetype('assets/font.ttf', size=61)
+    #draw.text((630, 540), f'ID: {id}', fill=(255, 255, 255), font=font)
+    #
+ #   draw.text((630, 300), f'NAME: {user}', fill=(255, 255, 255), font=font)
+    draw.text((630, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
+#    draw.text((630, 150), f"{chatname}", fill=(225, 225, 225), font=welcome_font)
+  #  draw.text((630, 230), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
+
+    #
+    pfp_position = (48, 88)
     background.paste(pfp, pfp_position, pfp)
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
@@ -154,14 +142,14 @@ async def greet_new_member(_, member: ChatMemberUpdated):
     user = member.new_chat_member.user if member.new_chat_member else member.from_user
     
     # Add the modified condition here
-    if member.new_chat_member and not member.old_chat_member:
+    if member.new_chat_member and not member.old_chat_member and member.new_chat_member.status != "kicked":
     
         try:
             pic = await app.download_media(
                 user.photo.big_file_id, file_name=f"pp{user.id}.png"
             )
         except AttributeError:
-            pic = "VIPMUSIC/assets/upic.png"
+            pic = "assets/upic.png"
         if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
             try:
                 await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
@@ -171,20 +159,26 @@ async def greet_new_member(_, member: ChatMemberUpdated):
             welcomeimg = welcomepic(
                 pic, user.first_name, member.chat.title, user.id, user.username
             )
-            button_text = "❍ 𓆩 𝗦𝐓𝐘𝐋𝐈𝐒𝐇 ⌯ 𝗡𝐀𝐌𝐄 𓆪 ❍"
-            add_button_text = "❍ 𝐏𝐑𝐎𝐌𝐎𝐓𝐈𝐎𝐍 𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄 ❍"
-            deep_link = f"https://t.me/TG_NAME_STYLE"
-            add_link = f"https://t.me/TG_NAME_STYLE/4602"
+            button_text = "๏ ᴠɪᴇᴡ ɴᴇᴡ ᴍᴇᴍʙᴇʀ ๏"
+            add_button_text = "๏ ᴋɪᴅɴᴀᴘ ᴍᴇ ๏"
+            deep_link = f"tg://openmessage?user_id={user.id}"
+            add_link = f"https://t.me/{app.username}?startgroup=true"
             temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
                 member.chat.id,
                 photo=welcomeimg,
                 caption=f"""
-╔═.✵.════════════════╗
-❍ 𝐍ᴀᴍᴇ ➥  {user.mention}
-❍ 𝐔sᴇʀɴᴀᴍᴇ ➥  @{user.username}
-❍ 𝐔sᴇʀ 𝐈ᴅ ➥  {user.id}
-❖ 𝐏ᴏᴡᴇʀᴇᴅ 𝐁ʏ ➥ ๛[❤️‍🔥 • 𝛚𝛐𝛚 • ❤️‍🔥](https://t.me/ll_SARKAR_MERA_BABU_ll)
-╚════════════════.✵.═╝
+**⎊────☵ 𝐖ᴇʟᴄᴏᴍᴇ ☵────⎊**
+
+**▬▭▬▭▬▭▬▭▬▭▬▭**
+
+**☉ 𝐍ᴀᴍᴇ ⧽** {user.mention}
+**☉ 𝐈ᴅ ⧽** `{user.id}`
+**☉ 𝐔_𝐍ᴀᴍᴇ ⧽** @{user.username}
+**☉ 𝐓ᴏᴛᴀʟ 𝐌ᴇᴍʙᴇʀs ⧽** {count}
+**☉ ❖ 𝐏ᴏᴡᴇʀᴇᴅ 𝐁ʏ ➥ ๛[❤️‍🔥 • 𝛚𝛐𝛚 • ❤️‍🔥](https://t.me/ll_SARKAR_MERA_BABU_ll)
+**▬▭▬▭▬▭▬▭▬▭▬▭**
+
+**⎉────▢✭ 侖 ✭▢────⎉**
 """,
                 reply_markup=InlineKeyboardMarkup([
                     [InlineKeyboardButton(button_text, url=deep_link)],
@@ -193,6 +187,3 @@ async def greet_new_member(_, member: ChatMemberUpdated):
             )
         except Exception as e:
             LOGGER.error(e)
-
-
-      
